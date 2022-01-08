05c90f74c0f7d3e762d94f0509652a2947177216
Renann Souza <renann_puera@hotmail.com>
2022-01-08 14:07:31 -0300

    install styled-components

--- App.tsx
+++ App.tsx
@@ -1,10 +1,6 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { Home } from './src/screens/Home';
 
 export default function App() {
-  return (
-    <View>
-      <Text>Hello World!</Text>
-    </View>
-  );
+  return <Home />;
 }
