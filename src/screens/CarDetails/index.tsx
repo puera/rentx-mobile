d8fa1db626a948b5a5208498975653b5d531b966
Renann Souza <renann_puera@hotmail.com>
2022-01-09 14:41:50 -0300

    create Accessory/Button/ImageSlider component

--- src/screens/CarDetails/index.tsx
+++ src/screens/CarDetails/index.tsx
@@ -1,7 +1,32 @@
 import React from 'react';
+import { Accessory } from '../../components/Accessory';
 import { BackButton } from '../../components/BackButton';
+import { ImageSlider } from '../../components/ImageSlider';
 
-import { Container, Header } from './styles';
+import SpeedSvg from '../../assets/speed.svg';
+import AccelerationSvg from '../../assets/acceleration.svg';
+import ForceSvg from '../../assets/force.svg';
+import GasolineSvg from '../../assets/gasoline.svg';
+import ExchangeSvg from '../../assets/exchange.svg';
+import PeopleSvg from '../../assets/people.svg';
+
+import {
+  Container,
+  Header,
+  CarImages,
+  Content,
+  Details,
+  Description,
+  Brand,
+  Name,
+  Rent,
+  Period,
+  Price,
+  About,
+  Accessories,
+  Footer,
+} from './styles';
+import { Button } from '../../components/Button';
 
 export function CarDetails() {
   return (
@@ -9,6 +34,42 @@ export function CarDetails() {
       <Header>
         <BackButton onPress={() => {}} />
       </Header>
+      <CarImages>
+        <ImageSlider
+          imagesUrl={['https://cdn.picpng.com/audi/audi-red-a5-28597.png']}
+        />
+      </CarImages>
+      <Content>
+        <Details>
+          <Description>
+            <Brand>Audi</Brand>
+            <Name>RS 5 Coupé</Name>
+          </Description>
+
+          <Rent>
+            <Period>Ao dia</Period>
+            <Price>R$ 580</Price>
+          </Rent>
+        </Details>
+
+        <Accessories>
+          <Accessory name="380Km/h" icon={SpeedSvg} />
+          <Accessory name="3.2s" icon={AccelerationSvg} />
+          <Accessory name="800 HP" icon={ForceSvg} />
+          <Accessory name="Gasolina" icon={GasolineSvg} />
+          <Accessory name="Auto" icon={ExchangeSvg} />
+          <Accessory name="2 pessoas" icon={PeopleSvg} />
+        </Accessories>
+
+        <About>
+          Lorem, ipsum dolor sit amet consectetur adipisicing elit. Atque illum
+          quis aut ratione earum cum eos quisquam quo eius modi, placeat, maxime
+          commodi! Quam rem ea, mollitia numquam doloremque voluptatem!
+        </About>
+      </Content>
+      <Footer>
+        <Button title="Confirmar" />
+      </Footer>
     </Container>
   );
 }
