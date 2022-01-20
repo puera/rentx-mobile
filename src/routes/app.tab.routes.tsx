fbce940cafc67e4a1ccd4634bb8ddbc3ee93f6ee
Renann Souza <renann_puera@hotmail.com>
2022-01-20 01:42:27 -0300

    customize tabbar

--- src/routes/app.tab.routes.tsx
+++ src/routes/app.tab.routes.tsx
@@ -1,23 +1,61 @@
 import React from 'react';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { useTheme } from 'styled-components';
+import { Platform } from 'react-native';
 import { AppStackRoutes } from './app.stack.routes';
 import { Profile } from '../screens/Profile';
 import { MyCars } from '../screens/MyCars';
-import { Home } from '../screens/Home';
+
+import HomeSvg from '../assets/home.svg';
+import PeopleSvg from '../assets/people.svg';
+import CarSvg from '../assets/car.svg';
 
 const { Navigator, Screen } = createBottomTabNavigator();
 
 export function AppTabRoutes() {
+  const theme = useTheme();
   return (
     <Navigator
       initialRouteName="AppStackRoutes"
       screenOptions={{
         headerShown: false,
+        tabBarActiveTintColor: theme.colors.main,
+        tabBarInactiveTintColor: theme.colors.text_detail,
+        tabBarShowLabel: false,
+        tabBarStyle: {
+          paddingVertical: Platform.OS === 'ios' ? 20 : 0,
+          height: 78,
+          backgroundColor: theme.colors.background_primary,
+        },
       }}
     >
-      <Screen name="AppStackRoutes" component={AppStackRoutes} />
-      <Screen name="MyCars" component={MyCars} />
-      <Screen name="Profile" component={Profile} />
+      <Screen
+        name="AppStackRoutes"
+        component={AppStackRoutes}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <HomeSvg height={24} width={24} fill={color} />
+          ),
+        }}
+      />
+      <Screen
+        name="MyCars"
+        component={MyCars}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <CarSvg height={24} width={24} fill={color} />
+          ),
+        }}
+      />
+      <Screen
+        name="Profile"
+        component={Profile}
+        options={{
+          tabBarIcon: ({ color }) => (
+            <PeopleSvg height={24} width={24} fill={color} />
+          ),
+        }}
+      />
     </Navigator>
   );
 }
